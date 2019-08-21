df55c7924f56ba799eab3f8e260dd4135041ccd7
George Ruiz <georuiz817@gmail.com>
2019-08-21 06:06:14 +0000

    first commit

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,44 @@
+class RestaurantsController < ApplicationController
+ get '/restaurants' do
+    @restaurants = Restaurant.all
+    erb :'restaurants/index'
+  end
+
+     get '/restaurants/new' do
+        erb :'restaurants/new'
+    end
+
+    post '/restaurants' do
+    @restaurant = Restaurant.create(params)
+    redirect '/restaurants/#{@restaurant.id}'
+    end 
+
+
+    get '/restaurants/:id' do
+    @restaurant = Restaurant.find(params[:id])
+    erb :'restaurants/show'
+  end 
+  
+    get '/restaurants/:id/edit' do 
+        @restaurant = Restaurant.find(params[:id])
+        erb :edit
+    end 
+
+
+ patch '/restaurants/:id' do 
+        @restaurant = Restaurant.find(params[:id])
+        @restaurant.name = params[:name]
+        @restaurant.location = params[:location]
+        @restaurant.wifi_avaliable = params[:wifi_avaliable]
+        @restaurant.save
+        redirect '/restaurants'
+    end 
+
+    delete '/restaurants/:id' do 
+        Restaurant.destroy(params[:id])
+        redirect '/restaurants'
+    end 
+
+    
+
+  end 
---
+++ db/migrate/20190821060240_create_restaurants.rb
@@ -0,0 +1,10 @@
+class CreateRestaurants < ActiveRecord::Migration
+    def change
+      create_table :restaurants do |t|
+      t.string :name 
+      t.string :location
+      t.string :wifi_avaliable 
+      t.integer :user_id
+  end
+end
+end
